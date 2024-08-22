5f5121adff2272818385325c1171737a8d3244db
skyflyi <2716690230@qq.com>
2024-08-22 10:55:02 +0800

    edit

--- src/backbone/__init__.py
+++ src/backbone/__init__.py
@@ -12,18 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""hub config."""
-from src.deeplab_v3plus import DeepLabV3Plus
-
-
-def create_network(name, *args, **kwargs):
-    freeze_bn = True
-    num_classes = kwargs["num_classes"]
-    phase = kwargs["phase"]
-    if name == 'DeepLabV3plus_s16':
-        DeepLabV3plus_s16_network = DeepLabV3Plus(phase, num_classes, 16, freeze_bn)
-        return DeepLabV3plus_s16_network
-    if name == 'DeepLabV3plus_s8':
-        DeepLabV3plus_s8_network = DeepLabV3Plus(phase, num_classes, 8, freeze_bn)
-        return DeepLabV3plus_s8_network
-    raise NotImplementedError(f"{name} is not implemented in the repo")
+"""init SwinTransformers"""
+from .get_swin import swin_tiny_patch4_window7_224
